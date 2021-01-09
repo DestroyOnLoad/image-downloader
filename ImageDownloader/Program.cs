a40f53cbc2cde07784ffacca723b8a45f250a0c8
David Potter <destroyonload@gmail.com>
2021-01-09 22:12:11 +0800

    init commit Added: Initial logic.  In the middle of revising to accept a list of file names to download from the same URI.

---
+++ ImageDownloader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ImageDownloader
+{
+    class Program
+    {
+        static string remoteUri;
+        static string downloadRepository;
+        static List<string> fileNames;
+
+        static void Main(string[] args)
+        {
+            BuildImageDirectory();
+        }
+
+        private static void BuildImageDirectory()
+        {
+            fileNames = GetFileNames();
+            DownloadFiles();
+        }
+
+        private static void DownloadFiles()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile($"{remoteUri}", $"{downloadRepository}");
+            }
+        }
+
+        private static List<string> GetFileNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
